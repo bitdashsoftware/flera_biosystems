3dbc286f089a8c8bd30993fb821ab7cc2447f2dd
BitdashSoftware <bitdashsoftware@gmail.com>
2025-12-13 06:26:28 -0800

    hopefully fix the domain update issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://bitdashsoftware.github.io',
+  site: 'https://www.flerabiosystems.com',
   base: '/flera_biosystems'
 });
